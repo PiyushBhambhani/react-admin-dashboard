685b1c4a8585a2e164945032e86f2da5f62e7bf0
Piyush <bhambhanipiyush4@gmail.com>
2023-07-30 18:18:59 +0530

    Pages Components Routig initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,54 @@
+import Home from "./pages/home/home";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/login";
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
 
-  return (
-    <div>Hello World</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
