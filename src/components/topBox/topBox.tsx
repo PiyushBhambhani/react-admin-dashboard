50409d4bd740524979851f9df28b7c48e8f75db3
Piyush <bhambhanipiyush4@gmail.com>
2023-08-01 16:05:45 +0530

    Top Deals component Added

---
+++ src/components/topBox/topBox.tsx
@@ -0,0 +1,27 @@
+import { topDealUsers } from "../../data";
+import "./topBox.scss";
+
+const TopBox = () => {
+  return (
+    <div className="topBox">
+      <h1>Top Deals</h1>
+      <div className="list">
+        {topDealUsers.map((user) => (
+          <div className="listItem" key={user.id}>
+            <div className="user">
+              <img src={user.img} className="user-image"></img>
+              <div className="userTexts">
+                <span className="username">{user.username}</span>
+                <span className="email">{user.email}</span>
+              </div>
+            </div>
+
+            <span className="amount">${user.amount}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopBox;
