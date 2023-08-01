ad727a713cf3fb38e907f1c72d0cbf7962bca8d6
Piyush <bhambhanipiyush4@gmail.com>
2023-08-01 14:36:05 +0530

    Menu Design and data file added

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,6 +1,24 @@
+import { Link } from "react-router-dom";
 import "./menu.scss";
+import { menu } from "./../../data";
+
 const Menu = () => {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <div className="title">{item.title}</div>
+
+          {item.listItems.map((listItem) => (
+            <Link to={listItem.url} className="listItem">
+              <img src={listItem.icon} alt={listItem.title} />
+              <span className="listItemTitle">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
