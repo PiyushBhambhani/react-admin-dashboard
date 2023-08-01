e20dd31d0615d0d0fe9c320b269cda6e29886b4c
Piyush <bhambhanipiyush4@gmail.com>
2023-08-01 13:48:55 +0530

    Footer Design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import "./footer.scss";
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>IamAdmin</span>
+      <span>© Piyush Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
