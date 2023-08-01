10e6db382977170b098cb88bedb2eb96a698ae57
Piyush <bhambhanipiyush4@gmail.com>
2023-08-01 13:15:44 +0530

    Navbar design

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,34 @@
 import "./navbar.scss";
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="Company's Logo" />
+        <span>IamAdmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="Search" className="icon" />
+        <img src="/app.svg" alt="apps" className="icon" />
+        <img src="/expand.svg" alt="expand" className="icon" />
+        <div className="notification">
+          <img
+            src="/notifications.svg"
+            alt="notifications icon"
+            className="icon"
+          />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.webp?b=1&s=170667a&w=0&k=20&c=YQ_j83pg9fB-HWOd1Qur3_kBmG_ot_hZty8pvoFkr6A="
+            alt=""
+          />
+          <span>Jenny</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
