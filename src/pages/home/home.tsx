b42afbfa31b257b6b3588b9ff3f4f54c3e667ad9
Piyush <bhambhanipiyush4@gmail.com>
2023-08-07 12:47:38 +0530

    Adding area chart and pie chart

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -9,7 +9,9 @@ import {
 } from "../../data";
 import "./home.scss";
 import { chartBoxRevenue } from "./../../data";
-import BarChartBox from "../../components/BarChartBox.tsx/BarChartBox";
+import BarChartBox from "../../components/BarChartBox/BarChartBox";
+import PieChartBox from "../../components/PieChartBox/PieChartBox";
+import BigChartBox from "../../components/BigChartBox/BigChartBox";
 
 const Home = () => {
   return (
@@ -23,14 +25,16 @@ const Home = () => {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4"></div>
+      <div className="box box4">
+        <PieChartBox></PieChartBox>
+      </div>
       <div className="box box5">
         <ChartBox {...chartBoxRevenue} />
       </div>
       <div className="box box6">
         <ChartBox {...chartBoxConversion} />
       </div>
-      <div className="box box7">Box7</div>
+      <div className="box box7">{<BigChartBox />}</div>
       <div className="box box9">
         {/* <BarChartBox title={""} color={""} datakey={""} chartData={[]} />{" "} */}
         {/* or */}
